a8a79f59179246725d1d6dffc3ddf288f1df5869
Ishan Baniya <ibaniya98@gmail.com>
2021-10-07 22:12:27 -0400

    Add search input form

--- frontend/src/pages/search.jsx
+++ frontend/src/pages/search.jsx
@@ -1,5 +1,32 @@
+import { Container, Heading, Divider, Box } from '@chakra-ui/react';
+import { useState } from 'react';
+import SearchForm from '../components/search-form';
+
 function SearchPage() {
-  return <h1>Search Page</h1>;
+  const [loading, setLoading] = useState(false);
+  // const [restaurants, setRestaurants] = useState([]);
+
+  const findRestaurants = (latitude, longitude, radius) => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      console.log(latitude, longitude, radius);
+      // setRestaurants([]);
+    }, 3000);
+  };
+
+  return (
+    <Box>
+      <Container>
+        <Heading textAlign="center" my={5}>
+          Eat<span style={{ color: 'red' }}>Map</span>
+        </Heading>
+        <Divider my={5} />
+        <SearchForm onSubmit={findRestaurants} loading={loading} />
+      </Container>
+      <Divider mt={5} />
+    </Box>
+  );
 }
 
 export default SearchPage;
