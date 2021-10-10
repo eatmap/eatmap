1613acae98b0a316930e9bc97ab790f51736142b
Ishan Baniya <ibaniya98@gmail.com>
2021-10-10 14:09:09 -0400

    filter restaurants list on result

--- frontend/src/components/restaurant-list/index.jsx
+++ frontend/src/components/restaurant-list/index.jsx
@@ -1,5 +1,14 @@
-import { useContext } from 'react';
-import { Box, Flex, Text, Spinner, Divider } from '@chakra-ui/react';
+import { useContext, useEffect, useState } from 'react';
+import {
+  Box,
+  Flex,
+  Text,
+  Spinner,
+  Divider,
+  FormControl,
+  InputGroup,
+  Input,
+} from '@chakra-ui/react';
 import { InfoOutlineIcon } from '@chakra-ui/icons';
 
 import { RestaurantSearchContext } from '../../providers/RestaurantsContext';
@@ -8,10 +17,45 @@ import RestaurantCard from '../restaurant-card';
 function RestaurantList() {
   const { restaurants, loading } = useContext(RestaurantSearchContext);
 
+  const [filterText, setFilterText] = useState('');
+  useEffect(() => {
+    setFilterText('');
+  }, [loading, restaurants]);
+
+  const filteredRestaurants =
+    filterText && filterText.trim().length > 0
+      ? restaurants.filter((x) =>
+          x.name.toLowerCase().includes(filterText.toLowerCase()),
+        )
+      : restaurants;
+
+  const SearchFilter = (
+    <FormControl
+      mb={5}
+      isDisabled={loading}
+      bg="white"
+      mx="4"
+      my="6"
+      w="90%"
+      rounded="md"
+      boxShadow="md"
+    >
+      <InputGroup>
+        <Input
+          type="text"
+          placeholder="Filter restaurants"
+          value={filterText}
+          onChange={(x) => setFilterText(x.target.value)}
+        />
+      </InputGroup>
+    </FormControl>
+  );
+
   // No restaurant to display
-  if (restaurants.length === 0 || loading) {
+  if (filteredRestaurants.length === 0 || loading) {
     return (
       <Box minH="300px" position="relative">
+        {SearchFilter}
         <Box
           position="absolute"
           top="50%"
@@ -30,7 +74,9 @@ function RestaurantList() {
                 <InfoOutlineIcon textAlign="center" w={8} h={8} />
                 <Text fontSize="2xl">No Restaurants Found</Text>
                 <Divider />
-                <Text fontSize="sm">Try updating the search area</Text>
+                <Text fontSize="sm">
+                  Try updating the search area or search filter
+                </Text>
               </Box>
             )}
           </Flex>
@@ -41,7 +87,8 @@ function RestaurantList() {
 
   return (
     <Box overflowY="scroll" overflowX="hidden" maxH="65vh">
-      {restaurants.map(({ name, address, id, rating }) => {
+      {SearchFilter}
+      {filteredRestaurants.map(({ name, address, id, rating }) => {
         return (
           <RestaurantCard
             name={name}
