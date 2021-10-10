f640f0a55e9e9667151f589ac92015faf5c7b5c8
Ishan Baniya <ibaniya98@gmail.com>
2021-10-10 14:42:36 -0500

    Add basic 404 page

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -1,6 +1,29 @@
+import { Container, Heading, Text, Button, Divider } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
 function NoFoundPage() {
-    return <h1>404 Error</h1>;
-  }
-  
-  export default NoFoundPage;
-  
+  return (
+    <Container
+      h="100vh"
+      textAlign="center"
+      display="flex"
+      flexDir="column"
+      justifyContent="center"
+    >
+      <Heading size="4xl" letterSpacing={15}>
+        404
+      </Heading>
+      <Text fontSize="3xl">page not found</Text>
+
+      <Divider my="5" />
+      <Text fontSize="md" mb="5">
+        We’re sorry, the page you have looked for does not exist
+      </Text>
+      <Link to="/">
+        <Button>Go back to home page</Button>
+      </Link>
+    </Container>
+  );
+}
+
+export default NoFoundPage;
