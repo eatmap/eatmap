093cb3406fbfeffd497ffa555b924122aac8c5cc
Arthelon <hsing.daniel@gmail.com>
2021-10-11 15:58:29 -0400

    add login form
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,76 @@
+import { useForm } from 'react-hook-form';
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  Box,
+  Link,
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+} from '@chakra-ui/react';
+
+export default function LoginForm() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  function onSubmit(values) {
+    // TODO: connect with login API endpoint, redirect to / if auth is successful
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        resolve();
+      }, 3000);
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={errors.username}>
+        <FormLabel htmlFor="username">Username</FormLabel>
+        <Input
+          id="username"
+          placeholder="Username"
+          type="text"
+          {...register('username', {
+            required: 'Username is required',
+          })}
+        />
+        <FormErrorMessage>
+          {errors.username && errors.username.message}
+        </FormErrorMessage>
+      </FormControl>
+      <FormControl isInvalid={errors.password}>
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input
+          id="password"
+          placeholder="Password"
+          type="password"
+          {...register('password', {
+            required: 'Password is required',
+          })}
+        />
+        <FormErrorMessage>
+          {errors.password && errors.password.message}
+        </FormErrorMessage>
+      </FormControl>
+      <Box
+        display="flex"
+        alignItems="center"
+        mt={4}
+        justifyContent="space-between"
+      >
+        <Button colorScheme="teal" isLoading={isSubmitting} type="submit">
+          Submit
+        </Button>
+        <Link as={RouterLink} color="teal" to="/register">
+          Not a user?
+        </Link>
+      </Box>
+    </form>
+  );
+}
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,11 +1,15 @@
 import { Heading, Box } from '@chakra-ui/react';
 import HeaderText from '../components/HeaderText';
+import LoginForm from '../components/LoginForm';
 
 function LoginPage() {
   return (
-    <Box py={5}>
+    <Box py={5} display="flex" alignItems="center" flexDir="column">
       <HeaderText />
-      <Heading as="h3">Login</Heading>
+      <Heading size="lg" as="h2" my={4}>
+        Login
+      </Heading>
+      <LoginForm />
     </Box>
   );
 }
