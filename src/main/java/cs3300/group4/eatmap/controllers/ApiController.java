1633c69e993a9549ff0c695c9d6441b762ef3297
Martin Heberling <martin.heberling.dalies@gmail.com>
2021-10-12 19:29:52 -0400

    Added tests, Datastore class and api classes.

--- src/main/java/cs3300/group4/eatmap/controllers/ApiController.java
+++ src/main/java/cs3300/group4/eatmap/controllers/ApiController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import org.json.simple.JSONObject;
 
 @RestController
-public class TestController {
+public class ApiController {
 
     @GetMapping("/api/isup")
     public JSONObject isUp() {
@@ -17,16 +17,44 @@ public class TestController {
 
     @PostMapping("/api/login")
     public JSONObject requestLogin(@RequestParam String username, @RequestParam String password) {
-        JSONObject obj = new JSONObject();
-        obj.put("username", username);
-        obj.put("password", password);
-        System.out.println("Login request.");
-        Datastore.someMethod(username, password);
-        return obj;
+        // Make call to dataStore class
+        boolean successfulLogin = Datastore.checkLogin(username, password);
+
+        // Create JSON with specific objects.
+        JSONObject json = new JSONObject();
+        if (successfulLogin) {
+            json.put("loggedIn", true);
+            json.put("username", username);
+        } else {
+            json.put("loggedIn", false);
+        }
+
+        return json;
     }
 
     @PutMapping("/api/register")
     public JSONObject requestRegister(@RequestParam String username, @RequestParam String password) {
-        //TODO: Check if the username already exists
+
+        // Make call to dataStore class
+        boolean successfulRegistration = Datastore.registerNewUser(username, password);
+
+        // Create JSON with specific objects.
+        JSONObject json = new JSONObject();
+        if (successfulRegistration) {
+            json.put("registered", true);
+            json.put("loggedIn", true);
+            json.put("username", username);
+        } else {
+            json.put("registered", false);
+            json.put("loggedIn", false);
+        }
+
+        return json;
+    }
+
+    @GetMapping("/api/isAuthenticated")
+    public void isUserAuthenticated() {
+        //TODO: Change return to a JSON object.
+        //TODO: If the user is authenticated return a JSON with {authenticated: true} else with false.
     }
 }
