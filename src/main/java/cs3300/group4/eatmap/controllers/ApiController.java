c64bfc95d10ab9cc5481a07d00559ec207dbd6e8
Ishan Baniya <ibaniya98@gmail.com>
2021-10-14 01:03:31 -0400

    Refactor response for login api

--- src/main/java/cs3300/group4/eatmap/controllers/ApiController.java
+++ src/main/java/cs3300/group4/eatmap/controllers/ApiController.java
@@ -1,37 +1,51 @@
 package cs3300.group4.eatmap.controllers;
 
-import cs3300.group4.eatmap.authentication.Datastore;
+import cs3300.group4.eatmap.authentication.LoginForm;
 import cs3300.group4.eatmap.security.JwtAuth;
 import org.jose4j.lang.JoseException;
 import org.json.simple.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ApiController {
 
     @PostMapping("/api/login")
-    public JSONObject requestLogin(@RequestParam String username, @RequestParam String password) throws JoseException {
-        // Make call to dataStore class
+    public ResponseEntity<JSONObject> requestLogin(@RequestBody LoginForm login) throws Exception {
+        try {
+            String username = login.getUsername();
+            String password = login.getPassword();
 
-        // TODO: Uncomment line below and comment the "successfulLogin" line to use datastore.
+            // Make call to dataStore class
+            // TODO: Uncomment line below and comment the "successfulLogin" line to use datastore.
 //        boolean successfulLogin = Datastore.checkLogin(username, password);
-        boolean successfulLogin = true;
+            boolean successfulLogin = true;
 
-        // Create JSON with specific objects.
-        JSONObject json = new JSONObject();
-        if (successfulLogin) {
-            json.put("loggedIn", true);
-            json.put("username", username);
+            // Create JSON with specific objects.
+            JSONObject json = new JSONObject();
+            if (successfulLogin) {
+                json.put("loggedIn", true);
+                json.put("username", username);
 
-            JwtAuth jwtAuth = new JwtAuth();
-            String token = jwtAuth.getJwtToken(username);
+                JwtAuth jwtAuth = new JwtAuth();
+                String token = jwtAuth.getJwtToken(username);
 
-            json.put("token", token);
-        } else {
-            json.put("loggedIn", false);
+                json.put("token", token);
+                return new ResponseEntity<>(json, HttpStatus.OK);
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            JSONObject json = new JSONObject();
+            json.put("message", ex.getMessage());
+            json.put("status", HttpStatus.INTERNAL_SERVER_ERROR.toString());
+            return new ResponseEntity<>(json, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        return json;
+        // If no exception and was not a successful login, the credentials were incorrect
+        JSONObject json = new JSONObject();
+        json.put("message", "Please provide a valid credentials");
+        return new ResponseEntity<>(json, HttpStatus.BAD_REQUEST);
     }
 
     @PutMapping("/api/register")
